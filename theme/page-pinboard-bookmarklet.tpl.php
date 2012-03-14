cc827c1a7cfe3113d0401225a8d8819767b3ffc9
Dan Braghis <dan@zerolab.org>
2012-03-14 09:58:28 +0000

    Add bookmarklet page template and styles

---
+++ theme/page-pinboard-bookmarklet.tpl.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="<?php print $pinboard_css ?>" media="all" />
+</head>
+<body>
+  <div id="page">
+  <?php print $content; ?>
+  </div>
+</body>
+</html>
