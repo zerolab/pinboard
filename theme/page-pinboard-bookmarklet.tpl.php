99cbe7b14b102ae2f49c0063f7f25af0d14ef435
Dan Braghis <dan@zerolab.org>
2012-03-20 17:48:22 +0000

    Finish ajax form post and error handling

--- theme/page-pinboard-bookmarklet.tpl.php
+++ theme/page-pinboard-bookmarklet.tpl.php
@@ -10,44 +10,60 @@
   </div>
 <script>
   var s = document.getElementById("edit-submit");
+
   if (s) {
-  s.onClick = function() {
-      var x;
-      if (window.ActiveXObject) {
-        try {
-          x = new ActiveXObject("Msxml2.XMLHTTP");
-        }
-        catch(e) {
-          try {
-            x = new ActiveXObject("Microsoft.XMLHTTP")
-          }
-          catch(e) {}
-        }
-      }
-      else if (window.XMLHTTPRequest) {
-        x = new XMLHTTPRequest();
-      }
-
-      if (x) {
-        x.open("POST", document.getElementById("pinboard-form").action, true);
-        x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-        x.onreadystatechange = function() {
-          if (x.readyState === 4) {
-            if (x.status === 200) {
-              console.log(x.responseText);
-            }
-          }
-          else {
-            console.log("Something went wrong with the request");
-          }
-        }
-      }
-
-      return false;
+    if (s.attachEvent) {
+      s.attachEvent("onclick", "pinboard_submit");
+    }
+    else {
+      s.setAttribute("onclick", "pinboard_submit();return false;");
     }
   }
 
+function pinboard_submit() {
+  var x;
+  if (window.ActiveXObject) {
+    try {
+      x = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch(e) {
+      try {
+        x = new ActiveXObject("Microsoft.XMLHTTP")
+      }
+      catch(e) {}
+    }
+  }
+  else if (window.XMLHttpRequest) {
+    x = new XMLHttpRequest();
+  }
+
+  if (x) {
+    var data = "js=1&u=" + encodeURIComponent(document.getElementById("edit-url").value) + "&t=" + encodeURIComponent(document.getElementById("edit-title").value) + "&d=" + encodeURIComponent(document.getElementById("edit-description").value);
+    x.open("POST", document.getElementById("pinboard-form").action, true);
+    x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    x.onreadystatechange = function() {
+      if (x.readyState === 4) {
+        if (x.status === 200) {
+          var response = JSON.parse(x.responseText);
+
+          if (response.status === "success") {
+            window.location = response.message;
+          }
+          else {
+            var e = document.getElementById("messages");
+            e.innerHTML = response.message;
+            e.setAttribute("class", "error");
+          }
+        }
+      }
+    }
+
+    x.send(data);
+  }
+
+  return false;
+}
 </script>
 </body>
 </html>
