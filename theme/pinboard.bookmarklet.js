60155237faac185569ebb95d9f20cec884fb5f1d
Dan Braghis <dan@zerolab.org>
2012-03-21 11:51:17 +0000

    Tweak bookmarklet js + add close button
    Now the overlay will just hide when "close" is pressed.

--- theme/pinboard.bookmarklet.js
+++ theme/pinboard.bookmarklet.js
@@ -19,31 +19,46 @@ var frame_count = 0;
   dd = dd.length ? dd + '\u2026' : '';
 
   var t = document.title.replace(/(<[^>]*>)/g,''),
-      u = encodeURIComponent(location.href),
-      iframe_url = "http://lab/thebrowser.com/bookmark/let?" + "u=" + u + "&t=" + t + "&d=" + dd;
+      u = encodeURIComponent(location.href);
 
-  var div = document.getElementById('pinboard_bookmarklet');
-  if (div && typeof(div) !== "undefined") {
-    document.body.removeChild(div);
+  var o = document.getElementById('pinboard_overlay');
+  if (o && typeof(o) !== "undefined") {
+    //document.body.removeChild(o);
+    pinboard_show_overlay(o);
   }
+  else {
+    o = document.createElement("div");
+    o.id = "pinboard_overlay";
+    o.setAttribute("style", "background-color:rgba(0,0,0, 0.7); position:fixed; z-index:1024; width:100%; height:100%; top:0; left:0;transition:display 0s linear 0.5s;");
 
-  div = document.createElement("div");
-  div.id = "pinboard_bookmarklet";
-  div.setAttribute("style", "background-color:rgba(0,0,0, 0.7); position:fixed; z-index:1024; width:100%; height:100%; top:0; left:0;transition:display 0s linear 0.5s;");
-  i = document.createElement("iframe");
-  i.id = "pinboard_bookmarklet_frame";
-  i.src = iframe_url;
-  i.setAttribute("style", "background:#fff;border:1px solid #c5c5c5;left:50%;height:348px;margin:-225px 0 0 -175px;position:absolute;width:448px;top:50%;-webkit-box-shadow:4px 4px 12px #1f1f1f;-moz-box-shadow:4px 4px 12px #1f1f1f;box-shadow:4px 4px 12px #1f1f1f;");
-  i.setAttribute("scrolling", "no");
-  i.setAttribute('allowtransparency', 'true');
+    i = document.createElement("iframe");
+    i.src = "http://thebrowser.com/bookmark/let?u=" + u + "&t=" + t + "&d=" + dd;
+    i.setAttribute("style", "background:#fff;border:1px solid #d5d5d5;width:398px;height:298px;position:absolute;top:50%;left:50%;margin:-150px 0 0 -200px;");
+    i.setAttribute("border", "0");
+    i.setAttribute("allowtransparency", "true");
+    i.setAttribute("scrolling", "no");
+    i.setAttribute("onLoad", "frame_count++;pinboard_frame_load();");
 
-  div.appendChild(i);
-  document.body.insertBefore(div, document.body.firstChild);
-})();    i.setAttribute("onLoad", "frame_count++;pinboard_frame_load();");
+    var x = document.createElement("a");
+    x.href = "#";
+    x.innerHTML  = "Close";
+    x.setAttribute("onclick", "this.parentNode.style.display='none';return false;");
+    x.setAttribute("style", "background:#fff;display:block;color:#333;font-size:12px;left:50%;margin:-150px 0 0 150px;position:absolute;text-align:center;text-decoration:none;top:50%;width:50px;");
+
+    o.appendChild(i);
+    o.appendChild(x);
+    document.body.appendChild(o);
+  }
+})();
+
 function pinboard_frame_load(event) {
   var o = document.getElementById("pinboard_overlay");
 
   if (o && frame_count === 2) {
     setTimeout(function(){ document.body.removeChild(o);}, 1000);
   }
+}
+
+function pinboard_show_overlay(el) {
+  el.style.display = "";
 }
