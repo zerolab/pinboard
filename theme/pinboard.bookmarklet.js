b50920480894f5fc4454676991699c0df798eb1a
Dan Braghis <dan@zerolab.org>
2012-03-27 12:35:26 +0100

    Tweaks for http/https

--- theme/pinboard.bookmarklet.js
+++ theme/pinboard.bookmarklet.js
@@ -20,8 +20,8 @@ var frame_count = 0;
 
   var t = encodeURIComponent(document.title.replace(/(<[^>]*>)/g,'')),
       u = encodeURIComponent(location.href);
-
   var o = document.getElementById('pinboard_overlay');
+
   if (o && typeof(o) !== "undefined") {
     //document.body.removeChild(o);
     pinboard_show_overlay(o);
@@ -32,7 +32,7 @@ var frame_count = 0;
     o.setAttribute("style", "background-color:rgba(0,0,0, 0.7); position:fixed; z-index:1024; width:100%; height:100%; top:0; left:0;transition:display 0s linear 0.5s;");
 
     i = document.createElement("iframe");
-    i.src = "http://thebrowser.com/bookmark/let?u=" + u + "&t=" + t + "&d=" + dd;
+    i.src = "//thebrowser.com/bookmark/let?seed=" + (new Date().getTime()) + "&u=" + u + "&t=" + t + "&d=" + dd;
     i.setAttribute("style", "background:#fff;border:1px solid #d5d5d5;width:398px;height:298px;position:absolute;top:50%;left:50%;margin:-150px 0 0 -200px;");
     i.setAttribute("border", "0");
     i.setAttribute("allowtransparency", "true");
@@ -43,7 +43,7 @@ var frame_count = 0;
     x.href = "#";
     x.innerHTML  = "Close";
     x.setAttribute("onclick", "this.parentNode.style.display='none';return false;");
-    x.setAttribute("style", "background:#fff;display:block;color:#333;font-size:12px;left:50%;margin:-150px 0 0 150px;position:absolute;text-align:center;text-decoration:none;top:50%;width:50px;");
+    x.setAttribute("style", "background:#fff;display:block;color:#333;font-size:12px;left:50%;margin:-150px 0 0 145px;position:absolute;text-align:center;text-decoration:none;top:50%;width:50px;");
 
     o.appendChild(i);
     o.appendChild(x);
