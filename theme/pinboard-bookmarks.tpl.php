28c9f834c07c7879374d0987215081777e3ef098
Dan Braghis <dan@zerolab.org>
2012-04-20 19:19:16 +0100

    Fix bookmarklet visible for anonymous

--- theme/pinboard-bookmarks.tpl.php
+++ theme/pinboard-bookmarks.tpl.php
@@ -6,12 +6,15 @@
  * Bookmarks list template
  */
 
-if (!$bookmarks || empty($bookmarks)) : ?>
+if ($is_owner) : ?>
+  <?php if (!$bookmarks || empty($bookmarks)) : ?>
     <p id="pinboard-empty">You have no bookmarked items. Why not use the <?php print $bookmarklet ?> to get you started?</p>
-<?php else : ?>
+  <?php else : ?>
   <div id="pinboard-help">
-    <?php print $bookmarklet; ?>
+      <?php print $bookmarklet; ?>
   </div>
+  <?php endif; ?>
+<?php endif; ?>
   <ul class="<?php print $classes ?>">
   <?php foreach ($bookmarks as $bookmark) : ?>
     <li><?php print $bookmark ?></li>
@@ -20,4 +23,3 @@ if (!$bookmarks || empty($bookmarks)) : ?>
   <?php if ($pager) : ?>
     <?php print $pager; ?>
   <?php endif; ?>
-<?php endif; ?>
